99366532395fb7415360c55bec8f913312d98cf8
Roy Guns <royguns@hotmail.com>
2021-01-11 02:21:04 +0100

    added setter for setting squares array via an int array parameter

--- src/main/java/be/roy/sudoku_solver/services/model/SudokuSquareGrid.java
+++ src/main/java/be/roy/sudoku_solver/services/model/SudokuSquareGrid.java
@@ -9,22 +9,29 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SudokuSquareGrid {
   private final SudokuSquare[] squares = new SudokuSquare[9];
-  
+
   /**
    * Set the value of square in the 3x3 grid
+   * 
    * @param squareId Ranges from 0-8; representing the 9 squares in the 3x3 grid
-   *                 [0,1,2]
-   *                 [3,4,5] 
-   *                 [6,7,8]
-   * @param value represents the value that goes in a certain square
+   *                 [0,1,2] [3,4,5] [6,7,8]
+   * @param value    represents the value that goes in a certain square
    */
   public void setSquareValue(int squareId, int value) {
-    if (squareId > 9 && value > 9) {
+    if (squareId <= 9 && value <= 9) {
       this.squares[squareId].setValue(value);
     }
   }
 
-  public void setHints(int squareId, List<Integer> hints){
+  public void setSquares(int[] squareValues) {
+    for (int i = 0; i < squareValues.length; i++) {
+      if (squareValues[i] <= 9) {
+        this.squares[i].setValue(squareValues[i]);
+      }
+    }
+  }
+
+  public void setHints(int squareId, List<Integer> hints) {
     this.squares[squareId].setHints(hints);
   }
 }
