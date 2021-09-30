fed3aa6a1df592d18d4867da5b211266687ede49
Roy65447 <royguns@hotmail.com>
2021-09-30 03:24:31 +0200

    first commit

---
+++ src/test/java/be/roy/sudokusolver/SudokusolverApplicationTests.java
@@ -0,0 +1,13 @@
+package be.roy.sudokusolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokusolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
