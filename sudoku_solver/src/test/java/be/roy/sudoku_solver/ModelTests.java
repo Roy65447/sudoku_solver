cc3e04442bd75fc42f017e094e45cddded72b4b1
Roy65447 <royguns@hotmail.com>
2021-10-01 04:16:50 +0200

    Update ModelTests.java

--- sudoku_solver/src/test/java/be/roy/sudoku_solver/ModelTests.java
+++ sudoku_solver/src/test/java/be/roy/sudoku_solver/ModelTests.java
@@ -11,14 +11,37 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import be.roy.models.Square;
+import be.roy.models.Sudoku;
 
 @SpringBootTest
 public class ModelTests {
     private Square square;
+    private Sudoku sudoku;
 
     @BeforeEach
     void initObjects() {
         square = new Square();
+
+        Square[][] unsolved = {
+                { new Square(0), new Square(0), new Square(0), new Square(2), new Square(6), new Square(0),
+                        new Square(7), new Square(0), new Square(1) },
+                { new Square(6), new Square(8), new Square(0), new Square(0), new Square(7), new Square(0),
+                        new Square(0), new Square(9), new Square(0) },
+                { new Square(1), new Square(9), new Square(0), new Square(0), new Square(0), new Square(4),
+                        new Square(5), new Square(0), new Square(0) },
+                { new Square(8), new Square(2), new Square(0), new Square(1), new Square(0), new Square(0),
+                        new Square(0), new Square(4), new Square(0) },
+                { new Square(0), new Square(0), new Square(4), new Square(6), new Square(0), new Square(2),
+                        new Square(9), new Square(0), new Square(0) },
+                { new Square(0), new Square(5), new Square(0), new Square(0), new Square(0), new Square(3),
+                        new Square(0), new Square(2), new Square(8) },
+                { new Square(0), new Square(0), new Square(9), new Square(3), new Square(0), new Square(0),
+                        new Square(0), new Square(7), new Square(4) },
+                { new Square(0), new Square(4), new Square(0), new Square(0), new Square(5), new Square(0),
+                        new Square(0), new Square(3), new Square(6) },
+                { new Square(7), new Square(0), new Square(3), new Square(0), new Square(1), new Square(8),
+                        new Square(0), new Square(0), new Square(0) } };
+        sudoku = new Sudoku(unsolved);
     }
 
     @Test
@@ -44,5 +67,13 @@ public class ModelTests {
         square.setValue(6);
         assertEquals(0, square.getHints().size());
         assertEquals(6, square.getValue());
+    }
+
+    @Test
+    void testSudoku() {
+        assertEquals(6, sudoku.getSolved()[1][0].getValue());
+        assertEquals(2, sudoku.getSolved()[4][5].getValue());
+        assertEquals(3, sudoku.getSolved()[7][7].getValue());
+        System.out.println(sudoku.toString());
     }
 }
