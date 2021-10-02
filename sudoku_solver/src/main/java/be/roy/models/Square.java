0ea1ef73f9e66fe062ce84bcac856376251d0c32
Roy65447 <royguns@hotmail.com>
2021-10-02 04:00:48 +0200

    Update Square.java

--- sudoku_solver/src/main/java/be/roy/models/Square.java
+++ sudoku_solver/src/main/java/be/roy/models/Square.java
@@ -31,7 +31,7 @@ public class Square {
     public void removeHint(int hintValue) {
         hints.removeIf(value -> value.equals(hintValue));
     }
-
+//TODO check usefullness
     public void addHint(int hintValue) {
         if (!hints.contains(hintValue) && hintValue < 10) {
             hints.add(hintValue);
